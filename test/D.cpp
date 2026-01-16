4695e739f5eabf1f66dfec819ae60e76cdfe040e
jiangescn <95997396+jiangescn@users.noreply.github.com>
2026-01-16 17:00:30 +0800

    Arch's commit

--- test/D.cpp
+++ test/D.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
 int main()
 {
-    int s[10] = {1};
-    for (int i = 0; i < 10; i++)
-    {
-        cout << s[i] << endl;
-    }
     
+
+    int a[5][5];
+    int *p = a[0];
+
+    printf("%d", *(p + 10));
+
+    map<int, int> mp;
+
+    for (auto f = mp.begin(); f != mp.end(); f++) 
+    {
+        int a = (*f).first;
+        f->first;
+    }
+    for (auto f: mp) 
+    {
+        f.first;
+    }
 }
