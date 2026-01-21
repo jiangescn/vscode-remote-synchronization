fdfa420eaaca188633884662dd16e95db679485a
jianges <jiangescn@users.noreply.github.com>
2026-01-21 17:55:15 +0800

    26-01-21拓扑

--- learn/图论入门/P_4089_USACO_17_DEC_The_Bovine_Shuffle_S(拓扑).cpp
+++ learn/图论入门/P_4089_USACO_17_DEC_The_Bovine_Shuffle_S(拓扑).cpp
@@ -4,11 +4,58 @@ using namespace std;
 
 void solve()
 {
-    int a, b;
-    cin >> a >> b;
-    cout << a + b << endl;
-    cout << "早上好" << endl;
+    int n;
+    cin >> n;
+    vector<int> to(n+1);
+    vector<int> ru(n+1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> to[i];
+        ru[to[i]]++;
+    }
 
+    // cout << "ru: " << endl;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     cout << ru[i] << " ";
+    // }
+
+    queue<int> q;
+    vector<bool> remove(n+1, false);
+    int ans = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if(ru[i] == 0)
+        {
+            q.push(i);
+        }
+    }
+
+    while(!q.empty())
+    {
+        int temp = q.front();
+        q.pop();
+        remove[temp] = true;
+
+        int v = to[temp];
+        ru[v]--;
+        if (ru[v] == 0)
+        {
+            q.push(v);
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if(remove[i] == false)
+        {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+    return;
 }
 
 signed main()
