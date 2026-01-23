338ac6fa816094991aff6969d83fba570170d396
jianges <2660998832@qq.com>
2026-01-23 21:41:40 +0800

    26-1-23牛客练习赛148

--- compare.cpp
+++ compare.cpp
@@ -7,32 +7,6 @@ int main()
     int n, k;
     if (!(cin >> n >> k))
         return 0;
-    vector<int> m(n);
-    ll M = 0, P = 0;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> m[i];
-        M += m[i];
-        P += (ll)m[i] * (i + 1);
-    }
-    int N = 1 << n;
-    for (int mask = 0; mask < N; ++mask)
-    {
-        if (__builtin_popcount(mask) != k)
-            continue;
-        ll MS = 0, PS = 0;
-        for (int i = 0; i < n; i++)
-            if (mask & (1 << i))
-            {
-                MS += m[i];
-                PS += (ll)m[i] * (i + 1);
-            }
-        if (PS * M == P * MS)
-        {
-            cout << "Yes\n";
-            return 0;
-        }
-    }
-    cout << "No\n";
+    
     return 0;
 }
