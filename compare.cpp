e451c39aa507374e2775c847e5b7db33c6db2a24
jianges <2660998832@qq.com>
2026-02-01 12:35:50 +0800

    new win

--- compare.cpp
+++ compare.cpp
@@ -6,43 +6,45 @@ void solve()
 {
     int n;
     cin >> n;
-    vector<int> a(n + 1), b(n + 1);
-    for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    for (int i = 1; i <= n; i++)
-        cin >> b[i];
+    vector<int> s(n + 1);
+    int sum = 0;
 
-    ranges::sort(a | views::drop(1), greater<int>());
-
-    int ans = 0;
-    int idx = 1;
-    int mn = 1e11;
     for (int i = 1; i <= n; i++)
     {
-        int cnt = b[i];
-        if (n - idx + 1 < cnt)
-            break;
-        while (idx <= n && cnt--)
-        {
-            mn = a[idx];
-            idx++;
-        }
-        ans = max(ans, i * mn);
-        if (idx > n)
-            break;
-        // cout << mn << ' ' << i << ' ' << ans << '\n';
+        cin >> s[i];
     }
 
-    cout << ans << '\n';
+    // 从左到右约束：s[i] <= s[i-1] + 1
+    for (int i = 2; i <= n; i++)
+    {
+        if (s[i] > s[i - 1] + 1)
+        {
+            sum += s[i] - (s[i - 1] + 1);
+            s[i] = s[i - 1] + 1;
+        }
+    }
+
+    // 从右到左约束：s[i] <= s[i+1] + 1
+    for (int i = n - 1; i >= 1; i--)
+    {
+        if (s[i] > s[i + 1] + 1)
+        {
+            sum += s[i] - (s[i + 1] + 1);
+            s[i] = s[i + 1] + 1;
+        }
+    }
+
+    cout << sum << '\n';
 }
 
 signed main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int _ = 1;
-    cin >> _;
-    while (_--)
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
     {
         solve();
     }
